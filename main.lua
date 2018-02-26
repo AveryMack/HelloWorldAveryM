60539b65c88b4ddfd451466b189573b7490a592f
Avery Mack <avery.mack@ocsbstudent.ca>
2018-02-26 10:27:15 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------------------------
+--Name: Avery Mack
+-- Description: This program displays Hello, World to the iPad simulator AND the console. It
+-- also changes the text and backround colour on the simulator.
+-- 
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+
+-- Your code here
+--print my name to the console
+print ("Hello Avery Mack!")
+
+-- hide the status bar
+display.setStatusBar (display.HiddenStatusBar)
+
+-- change background colour
+display.setDefault ("background", 145/255, 168/255, 228/255)
+
+-- declare a local variable
+local textObject
+
+-- create the text object at position (300,500) with a size of 50
+textObject = display.newText ("Hello Avery Mack", 50, 100, nil, 50)
+
+-- anchor the textObject from the top left corner
+textObject.anchorX = 0
+textObject.anchorY = 0
+
+-- set the colour of the text object
+textObject: setFillColor (255, 255, 255)
+
+-- create another text variable
+local textObjectSignature
+
+-- sign my name
+textObjectSignature = display.newText ("By Avery Mack", display.contentCenterX, display.contentCenterX, nil, 25)
+
